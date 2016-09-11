d41deebfcdd42f9b8f75c0ea4bc79f8da425da5f
Dmytro <marchukdimka@mail.ru>
2016-09-11 16:17:34 +0300

    Implement grid layout

--- app/src/main/java/com/example/dmytro/gameoflife/MainActivity.java
+++ app/src/main/java/com/example/dmytro/gameoflife/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.dmytro.gameoflife;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
 
+public class MainActivity extends AppCompatActivity {
+    private PixelGridView grid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grid = (PixelGridView) findViewById(R.id.grid);
+        grid.setNumColumns(15);
     }
+
 }
---
+++ app/src/main/java/com/example/dmytro/gameoflife/PixelGridView.java
@@ -0,0 +1,104 @@
+package com.example.dmytro.gameoflife;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Dmytro on 11.09.2016
+ */
+public class PixelGridView extends View {
+    private int numColumns = 20, numRows;
+    private float cellWidth, cellHeight;
+    private Paint blackPaint = new Paint();
+    private boolean[][] cellChecked;
+
+    public PixelGridView(Context context) {
+        this(context, null);
+    }
+
+    public PixelGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        blackPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public void setNumColumns(int numColumns) {
+        this.numColumns = numColumns;
+        calculateDimensions();
+    }
+
+    public int getNumColumns() {
+        return numColumns;
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        calculateDimensions();
+    }
+
+    private void calculateDimensions() {
+
+        cellWidth = (float) getWidth() / (float) numColumns;
+        cellHeight = cellWidth;
+        numRows = Math.round(getHeight() / cellHeight);
+        cellChecked = new boolean[numColumns][numRows];
+        float overCellsSpace = getHeight() - (cellHeight * numRows);
+        if (overCellsSpace != 0)
+            setLayoutParams(new RelativeLayout.LayoutParams(getWidth(), Math.round(getHeight() - overCellsSpace)));
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+
+        if (numColumns == 0 || numRows == 0) {
+            return;
+        }
+
+        int width = getWidth();
+        int height = getHeight();
+
+        for (int i = 0; i < numColumns; i++) {
+            for (int j = 0; j < numRows; j++) {
+                if (cellChecked[i][j]) {
+
+                    canvas.drawRect(i * cellWidth, j * cellHeight,
+                            (i + 1) * cellWidth, (j + 1) * cellHeight,
+                            blackPaint);
+                }
+            }
+        }
+
+        for (int i = 1; i < numColumns; i++) {
+            canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, blackPaint);
+        }
+
+        for (int i = 1; i < numRows; i++) {
+            canvas.drawLine(0, i * cellHeight, width, i * cellHeight, blackPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            int column = (int) (event.getX() / cellWidth);
+            int row = (int) (event.getY() / cellHeight);
+
+            cellChecked[column][row] = !cellChecked[column][row];
+            invalidate();
+        }
+
+        return true;
+    }
+}
